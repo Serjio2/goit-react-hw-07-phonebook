39ca1dfa528c835e2d0214797369582a6884a2aa
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-09-28 00:45:56 +0300

    Add operations

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,14 +1,40 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { fetchContacts } from './contactsApi';
+import axios from 'axios';
 
-export const getAllContacts = createAsyncThunk(
-  'contacts/getContacts',
-  async (_, contactsAPI) => {
+axios.defaults.baseURL = 'https://650f4c6354d18aabfe99ff49.mockapi.io/api/v1';
+
+export const getContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
     try {
-      const response = await fetchContacts();
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async ({ name, phone }, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', { name, phone });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${id}`);
       return response;
     } catch (error) {
-      return contactsAPI.rejectWithValue(error.message);
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
